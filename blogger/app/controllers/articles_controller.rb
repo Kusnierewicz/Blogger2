d92c4561a4a9de54683056aafa9c984be6e0779d
Kusnierewicz <kusnierewicz@gmail.com>
2014-08-02 23:52:02 +0200

    section I1 finished

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
@@ -7,6 +9,33 @@ class ArticlesController < ApplicationController
 		
 	end
 	def new
+		@article = Article.new
+	end
+	def create
+		@article = Article.new(article_params)
+		@article.save
 
+		flash.notice = "Article '#{@article.title}' was Created!"
+
+		redirect_to article_path(@article)
+	end
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		flash.notice = "Article '#{@article.title}' was Destroyed!"
+
+		redirect_to articles_path
+	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated!"
+
+		redirect_to article_path(@article)
 	end
 end
